29e012863d1891de88c6c10f17fb06a8efae657d
James Wilson <treppiejames@hotmail.com>
2026-09-15 14:46:28 +1200

    Add minor touches

--- index.js
+++ index.js
@@ -4,30 +4,47 @@ import displayBoard from './displayBoard.js'
 import randomBoard from './randomBoard.js'
 
 let isRunning = false
+let intervalId = null
 
 const startBtn = document.getElementById('start-btn')
 const pauseBtn = document.getElementById('pause-btn')
 const resetBtn = document.getElementById('reset-btn')
 const sizeSelect = document.getElementById('grid-size')
 
-const size = 50
+let size = 50
 const refreshInterval = 100
 
 let board = randomBoard(createBoard(size))
+
+displayBoard(board)
 
 function startGame() {
   if (isRunning) return
   isRunning = true
   startBtn.disabled = true
   pauseBtn.disabled = false
-  gameLoop()
+
+  intervalId = setInterval(() => {
+    let theNextBoard = nextBoard(board)
+    displayBoard(theNextBoard)
+
+    if (boardIsStable(board, theNextBoard)) {
+      pauseGame()
+      console.log('Simulation stabilized')
+    }
+    board = theNextBoard
+  }, refreshInterval)
 }
 
 function pauseGame() {
   isRunning = false
   startBtn.disabled = false
   pauseBtn.disabled = true
-  // if (anima)
+
+  if (intervalId) {
+    clearInterval(intervalId)
+    intervalId = null
+  }
 }
 
 function resetGame() {
@@ -36,23 +53,15 @@ function resetGame() {
   displayBoard(board)
 }
 
+function updateSize(event) {
+  size = parseInt(event.target.value, 10)
+  resetGame()
+}
+
 startBtn.addEventListener('click', startGame)
 pauseBtn.addEventListener('click', pauseGame)
 resetBtn.addEventListener('click', resetGame)
 sizeSelect.addEventListener('change', updateSize)
-
-// Instead of process.exit(), we save the timer ID to clear it later
-const gameLoop = setInterval(() => {
-  displayBoard(board)
-  let theNextBoard = nextBoard(board)
-
-  if (boardIsStable(board, theNextBoard)) {
-    clearInterval(gameLoop) // Stops the timer safely in the browser
-    console.log('Simulation stabilized.')
-  }
-
-  board = theNextBoard
-}, refreshInterval)
 
 function boardIsStable(thisBoard, thatBoard) {
   return JSON.stringify(thisBoard) === JSON.stringify(thatBoard)
